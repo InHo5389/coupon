286a5cebc55c6b730b3e574377330a075d4a2e97
JEONG <qheogus55@naver.com>
2024-05-20 18:10:38 +0900

    [#1] feat: 쿠폰,쿠폰 이슈 엔티티 생성

---
+++ coupon-core/src/main/java/couponcore/model/Coupon.java
@@ -0,0 +1,44 @@
+package couponcore.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private CouponType couponType;
+
+    private Integer totalQuantity;
+
+    @Column(nullable = false)
+    private int issuedQuantity;
+
+    @Column(nullable = false)
+    private int discountAmount;
+
+    @Column(nullable = false)
+    private int minAvailableAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueStart;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueEnd;
+}
